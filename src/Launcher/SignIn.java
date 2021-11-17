e1a95ded7094783d923ec0c71096232a376ce8d2
BM201149 <B.alex.moldoveanu@gmail.com>
2021-11-17 16:13:29 +0000

    Sign in panel look idea

--- src/Launcher/SignIn.java
+++ src/Launcher/SignIn.java
@@ -1,9 +1,10 @@
 package Launcher;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 
-public class SignIn{
+public class SignIn extends javax.swing.JFrame{
     Toolkit tk = Toolkit.getDefaultToolkit();
     int xSize = ((int) tk.getScreenSize().getWidth());
     int ySize = ((int) tk.getScreenSize().getHeight());
@@ -17,11 +18,36 @@ public class SignIn{
     private JButton launchGameBtn;
     private JLabel enterEmailLabel;
     private JLabel enterPasswordLabel;
+    private JPanel picturePanel;
 
     public static JFrame frame = new JFrame("SurvivalGame Launcher");
 
     public SignIn(){
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setSize(new Dimension(1000,700));
+        this.setLocation(xSize/4,ySize/4-100);
+        this.add(signInPanel);
+        this.setVisible(true);
+
         signInPanel.setPreferredSize(new Dimension(xSize/4,ySize/4));
+        signInPanel.setForeground(new Color(255, 255, 255));
+
+        emailTf.setBorder(BorderFactory.createLineBorder(new Color(255, 255, 255)));
+        passwordTf.setBorder(BorderFactory.createLineBorder(new Color(255, 255, 255)));
+        enterEmailLabel.setForeground(new Color(0,0,0));
+        enterPasswordLabel.setForeground(new Color(0,0,0));
+        signInBtn.setBackground(new Color(255, 255, 255));
+        signInBtn.setForeground(new Color(0,0,0));
+        signInBtn.setBorder(BorderFactory.createLineBorder(new Color(0,0,0)));
+        signUpBtn.setBackground(new Color(255, 255, 255));
+        signUpBtn.setForeground(new Color(0,0,0));
+        signUpBtn.setBorder(BorderFactory.createLineBorder(new Color(0,0,0)));
+        forgotPasswordBtn.setBackground(new Color(255, 255, 255));
+        forgotPasswordBtn.setForeground(new Color(0,0,0));
+        forgotPasswordBtn.setBorder(BorderFactory.createLineBorder(new Color(0,0,0)));
+
+
+        launchGameBtn.setVisible(false);
     }
 
 
