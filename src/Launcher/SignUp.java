9bc9f064ea07ada4f2af72ce09e39b12881814ea
BM201149 <B.alex.moldoveanu@gmail.com>
2021-12-01 16:12:09 +0000

    Sign up form

--- src/Launcher/SignUp.java
+++ src/Launcher/SignUp.java
@@ -2,88 +2,86 @@ package Launcher;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
-import javax.swing.border.Border;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
-public class SignIn extends javax.swing.JFrame{
+public class SignUp extends javax.swing.JFrame{
     Toolkit tk = Toolkit.getDefaultToolkit();
     int xSize = ((int) tk.getScreenSize().getWidth());
     int ySize = ((int) tk.getScreenSize().getHeight());
 
-    private JPanel signInPanel;
     private JTextField emailTf;
     private JPasswordField passwordTf;
-    private JButton signInBtn;
-    private JButton signUpBtn;
-    private JButton forgotPasswordBtn;
-    private JButton launchGameBtn;
     private JLabel enterEmailLabel;
     private JLabel enterPasswordLabel;
     private JPanel picturePanel;
     private JPanel LogoPicturePanel;
+    private JPanel signUpPanel;
+    private JButton backBtn;
+    private JButton registerBtn;
+    private JLabel confirmPasswordLb;
+    private JPasswordField confirmPasswordTf;
+    private JTextField accountNameTf;
+    private JLabel accountNameLb;
 
-
-
-    public SignIn(){
+    public SignUp(){
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setSize(new Dimension(1070,700));
         this.setLocation(xSize/4-100,ySize/4-100);
-        this.add(signInPanel);
+        this.add(signUpPanel);
         this.setVisible(true);
 
-        signInPanel.setPreferredSize(new Dimension(xSize/4,ySize/4));
-        signInPanel.setForeground(new Color(255, 255, 255));
+        signUpPanel.setPreferredSize(new Dimension(xSize/4,ySize/4));
+        signUpPanel.setForeground(new Color(255, 255, 255));
 
         emailTf.setBorder(BorderFactory.createLineBorder(new Color(255, 255, 255)));
         emailTf.setBackground(new Color(92, 91, 105, 255));
         emailTf.setForeground(new Color(255, 255, 255));
+        accountNameTf.setBorder(BorderFactory.createLineBorder(new Color(255, 255, 255)));
+        accountNameTf.setBackground(new Color(92, 91, 105, 255));
+        accountNameTf.setForeground(new Color(255, 255, 255));
         passwordTf.setBorder(BorderFactory.createLineBorder(new Color(255, 255, 255)));
         passwordTf.setBackground(new Color(92, 91, 105));
         passwordTf.setForeground(new Color(255, 255, 255));
+        confirmPasswordTf.setBorder(BorderFactory.createLineBorder(new Color(255, 255, 255)));
+        confirmPasswordTf.setBackground(new Color(92, 91, 105));
+        confirmPasswordTf.setForeground(new Color(255, 255, 255));
+
+        accountNameLb.setForeground(new Color(255, 255, 255));
         enterEmailLabel.setForeground(new Color(255, 255, 255));
         enterPasswordLabel.setForeground(new Color(255, 255, 255));
-        signInBtn.setBackground(new Color(92, 91, 105));
-        signInBtn.setForeground(new Color(255, 255, 255));
-        signInBtn.setBorder(BorderFactory.createLineBorder(new Color(255, 255, 255)));
-        signUpBtn.setBackground(new Color(92, 91, 105));;
-        signUpBtn.setForeground(new Color(255, 255, 255));
-        signUpBtn.setBorder(BorderFactory.createLineBorder(new Color(255, 255, 255)));
-        forgotPasswordBtn.setBackground(new Color(92, 91, 105));
-        forgotPasswordBtn.setForeground(new Color(255, 255, 255));
-        forgotPasswordBtn.setBorder(BorderFactory.createLineBorder(new Color(255, 255, 255)));
+        confirmPasswordLb.setForeground(new Color(255, 255, 255));
+
+        backBtn.setBackground(new Color(92, 91, 105));
+        backBtn.setForeground(new Color(255, 255, 255));
+        backBtn.setBorder(BorderFactory.createLineBorder(new Color(255, 255, 255)));
+        registerBtn.setBackground(new Color(92, 91, 105));;
+        registerBtn.setForeground(new Color(255, 255, 255));
+        registerBtn.setBorder(BorderFactory.createLineBorder(new Color(255, 255, 255)));
 
         picturePanel.setLayout(new FlowLayout());
         try {
-            System.out.println(picturePanel.getSize());
             BufferedImage myPicture = ImageIO.read(new File(("Images/Launcher/LauncherPanelPicture.png")));
             JLabel launcherImageLabel = new JLabel(new ImageIcon(myPicture));
             picturePanel.add(launcherImageLabel);
             launcherImageLabel.setPreferredSize(new Dimension(picturePanel.getWidth(), picturePanel.getHeight()));
+            launcherImageLabel.revalidate();
         } catch (IOException e) {
             e.printStackTrace();
         }
+
         LogoPicturePanel.setLayout(new FlowLayout());
         try {
             BufferedImage myLogoPicture = ImageIO.read(new File(("Images/Launcher/Logo.png")));
             JLabel launcherLogoLabel = new JLabel(new ImageIcon(myLogoPicture));
             LogoPicturePanel.add(launcherLogoLabel);
+            launcherLogoLabel.revalidate();
         }catch (IOException e){
             e.printStackTrace();
         }
 
-        signUpBtn.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                new SignUp();
-
-            }
-        });
-        launchGameBtn.setVisible(false);
     }
 
 
