dd1f16082b4f227bfb6d6bbd8a0ba64f731e52b8
BM201149 <B.alex.moldoveanu@gmail.com>
2021-12-07 12:52:58 +0000

    Still doing Insert INTO stuff

--- src/SurvivalGame/Repository.java
+++ src/SurvivalGame/Repository.java
@@ -3,6 +3,8 @@ package SurvivalGame;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Repository {
 
@@ -21,13 +23,41 @@ public class Repository {
 
     public static void inputSignUpDetails(String Name, String Email, String Password) {
         try {
+            Statement stmt = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
             String sql = "INSERT Login (Account_Name, Email, Password) VALUES ('" + Name +"','" + Email + "','" + Password + "')";
-            ResultSet justDoIt;
+            ResultSet rs = stmt.executeQuery(sql);
+
+            rs.close();
+            stmt.close();
+            con.close();
 
         }catch (Exception e) {
             System.out.println("Error in the repository: " + e);
         }
 
     }
+//    public static void CreateAccount(String Name, String Email, String Password) {
+//        try {
+//            Connection con = DriverManager.getConnection("jdbc:mySql//localhost:1527/SurvivalGameDatabase", "", "");
+//            Statement stmt = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+//
+//            String sql = "Select * From Login";
+//            ResultSet rs = stmt.executeQuery(sql);
+//            int newIdNum = -1;
+//            if (rs.last()) {
+//                newIdNum = rs.getInt("AccountID") + 1;
+//            }
+//
+//            sql = "Insert into Login (Account_Name, Email, Password) Values ('" + Name +"','" + Email + "','" + Password + "')";
+//            stmt.executeUpdate(sql);
+//
+//            rs.close();
+//            stmt.close();
+//            con.close();
+//
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//    }
 
 }
